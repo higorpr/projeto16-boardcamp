00c49df723de1f4bc806794c1e9c72b3f188af95
higorpr <higorpr@gmail.com>
2022-12-11 03:55:41 -0300

    feat:GET games completed

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -2,23 +2,34 @@ import { connection } from "../database/db.js";
 
 export async function getGames(req, res) {
 	let games;
+	let { name } = req.query;
+	let gamesResponse;
+
 	try {
-		const gamesResponse = await connection.query(`
-        SELECT
-            games.id, 
-            games.name, 
-            games.image, 
-            games."stockTotal", 
-            games."categoryId",
-            games."pricePerDay",
-            categories.name AS "categoryName"
-        FROM
-            games
-        JOIN
-            categories
-        ON
-            games."categoryId" = categories.id    
-    `);
+        const nameLowered = (name) ? name.toLowerCase() : '';
+			gamesResponse = await connection.query(
+				`
+            SELECT
+                games.id, 
+                games.name, 
+                games.image, 
+                games."stockTotal", 
+                games."categoryId",
+                games."pricePerDay",
+                categories.name AS "categoryName"
+            FROM
+                games
+            JOIN
+                categories
+            ON
+                games."categoryId" = categories.id
+            WHERE
+                LOWER (games.name)
+            LIKE
+                $1||'%'
+        `,
+				[nameLowered]
+			);
 
 		games = gamesResponse.rows;
 
@@ -26,8 +37,11 @@ export async function getGames(req, res) {
 		console.log(err);
 		return res.sendStatus(500);
 	}
-	
+
 	res.status(200).send(games);
 }
 
-export function postGame(req, res) {}
+export function postGame(req, res) {
+    
+
+}
